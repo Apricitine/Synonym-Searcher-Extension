0ed7b529e3f40ff7f7f885aa348e4900f3b2019c
Cas3y <apricitinecas@gmail.com>
2022-03-30 18:00:47 -0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const textInputTag = document.querySelector("[Text-Input]");
+const submitTextTag = document.querySelector("[Submit-Input]");
+let submittedValue = null;
+console.log(`${textInputTag}`);
+
+textInputTag.addEventListener("input", userInput => {
+  submittedValue = userInput.target.value;
+  console.log(submittedValue);
+  return submittedValue;
+});
+submitTextTag.addEventListener("click", () => {
+  window.open(`https://www.google.com/search?q=${submittedValue}%20synonym`);
+});
